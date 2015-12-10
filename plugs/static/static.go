76c3a57ed5d2686c6c99b84b7b2d20cddc0af107
Alex Chen <alexanderchenmh@gmail.com>
2015-12-10 16:01:56 +0800

    add static

---
+++ plugs/static/static.go
@@ -0,0 +1,70 @@
+package static
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/AlexanderChen1989/xrest"
+	"golang.org/x/net/context"
+)
+
+type static struct {
+	dir    string
+	prefix string
+
+	fileHandler http.Handler
+
+	next xrest.Handler
+}
+
+// Dir set real directory to serve static files from
+func Dir(path string) func(*static) {
+	return func(s *static) {
+		s.dir = path
+	}
+}
+
+// Prefix set prefix to url to serve static files
+func Prefix(path string) func(*static) {
+	return func(s *static) {
+		s.prefix = path
+	}
+}
+
+// New create a new static file server
+// Default:
+//  dir -> ./static
+//  prefix -> /public
+func New(setups ...func(*static)) xrest.Plugger {
+	s := &static{
+		dir:    "./static",
+		prefix: "public",
+	}
+	for _, setup := range setups {
+		setup(s)
+	}
+
+	s.fileHandler = http.StripPrefix(
+		s.prefix,
+		http.FileServer(
+			http.Dir(s.dir),
+		),
+	)
+
+	return s
+}
+
+// ServeHTTP implements xrest.Handler
+func (s *static) ServeHTTP(ctx context.Context, res http.ResponseWriter, req *http.Request) {
+	if strings.HasPrefix(req.URL.Path, s.prefix) {
+		s.fileHandler.ServeHTTP(res, req)
+		return
+	}
+	s.next.ServeHTTP(ctx, res, req)
+}
+
+// Plug implements xrest.Plugger
+func (s *static) Plug(h xrest.Handler) xrest.Handler {
+	s.next = h
+	return s
+}
---
+++ plugs/static/static_test.go
@@ -0,0 +1,75 @@
+package static
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"golang.org/x/net/context"
+
+	"github.com/AlexanderChen1989/xrest"
+	"github.com/stretchr/testify/assert"
+)
+
+func equalByteSlice(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+var testCases = []struct {
+	file   string
+	exists bool
+}{
+	{"a.txt", true},
+	{"b.txt", true},
+	{"c.txt", true},
+	{"d.txt", false},
+}
+
+func TestStatic(t *testing.T) {
+	s := New(
+		Dir("test"),
+		Prefix("/public"),
+	)
+
+	for i := range testCases {
+		c := testCases[i]
+		pipe := xrest.NewPipeline()
+		pipe.Plug(s)
+		pipe.Plug(
+			xrest.HandleFunc(
+				func(ctx context.Context, res http.ResponseWriter, req *http.Request) {
+					if c.exists {
+						t.Error("should not be here\n")
+						return
+					}
+
+					t.Error("should be here\n")
+				},
+			),
+		)
+
+		server := httptest.NewServer(pipe.HTTPHandler())
+		res, err := http.Get(server.URL + "/public/" + c.file)
+		fmt.Println(err)
+		assert.Nil(t, err)
+		bsa, err := ioutil.ReadAll(res.Body)
+		assert.Nil(t, err)
+		res.Body.Close()
+		if c.exists {
+			bsb, err := ioutil.ReadFile("test/" + c.file)
+			assert.Nil(t, err)
+			assert.True(t, equalByteSlice(bsa, bsb))
+		}
+		server.Close()
+	}
+}
